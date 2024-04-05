c48db03b36b08d018ebdc2316c9daf79a5043a16
Михаил Шишов <145226164+mdshishov@users.noreply.github.com>
2024-04-05 13:40:36 +0300

    Task 1 step 1 completed

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 // task 1
-const tableParsing = (/* content */) => {
+const tableParsing = (fileContentStr) => {
+  const [keysArr, ...dataArr] = fileContentStr
+    .split('\n')
+    .filter((str) => str !== '')
+    .map((str) => str.split(';');
 
+  const objArr = dataArr
+    .map((valuesArr) => valuesArr.reduce((acc, value, i) => {
+      acc[keysArr[i]] = value;
+      return acc;
+    }, {}));
+
+  const meanRatingObjArr = objArr.map((obj) => {
+    const properKeysArr = keysArr.filter((key) => key.endsWith('_rating'));
+    const sum = properKeysArr.reduce((acc, key) => acc + Number(obj[key]), 0);
+    return {
+      name: obj.name,
+      deveoper: obj.developer,
+      mean: sum / properKeysArr.length,
+    };
+  });
+  const topMesObj = meanRatingObjArr
+    .sort(({ mean1 }, { mean2 }) => mean2 - mean1)
+    .at(0);
+
+  console.log(`General top messenger: ${topMesObj.name}, Owner: ${topMesObj.developer}`);
 };
 
 // task 2
